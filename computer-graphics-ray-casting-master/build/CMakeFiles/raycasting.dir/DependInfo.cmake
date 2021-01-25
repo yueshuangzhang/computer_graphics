b4fc8650d0813fbb36228fb8b4dd33ca994478cf
yueshuangzhang <yueshuang.zhang@mail.utoronto.ca>
2021-01-25 18:07:44 -0500

    build file

---
+++ computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/main.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/main.cpp.obj"
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/src/Plane.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/src/Plane.cpp.obj"
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/src/Sphere.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/src/Sphere.cpp.obj"
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/src/Triangle.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/src/Triangle.cpp.obj"
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/src/TriangleSoup.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/src/TriangleSoup.cpp.obj"
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/src/first_hit.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/src/first_hit.cpp.obj"
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/src/viewing_ray.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/src/viewing_ray.cpp.obj"
+  "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/src/write_ppm.cpp" "C:/Users/92972_lo8jks/Documents/GitHub/computer_graphics/computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/src/write_ppm.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../eigen"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ computer-graphics-ray-casting-master/build/CMakeFiles/raycasting.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raycasting.dir/main.cpp.obj"
+  "CMakeFiles/raycasting.dir/src/Plane.cpp.obj"
+  "CMakeFiles/raycasting.dir/src/Sphere.cpp.obj"
+  "CMakeFiles/raycasting.dir/src/Triangle.cpp.obj"
+  "CMakeFiles/raycasting.dir/src/TriangleSoup.cpp.obj"
+  "CMakeFiles/raycasting.dir/src/first_hit.cpp.obj"
+  "CMakeFiles/raycasting.dir/src/viewing_ray.cpp.obj"
+  "CMakeFiles/raycasting.dir/src/write_ppm.cpp.obj"
+  "libraycasting.dll.a"
+  "raycasting.exe"
+  "raycasting.exe.manifest"
+  "raycasting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raycasting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
