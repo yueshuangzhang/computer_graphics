e557eb621b7203d3631f1c5546ea3cbbb86d6a63
yueshuangzhang <yueshuang.zhang@mail.utoronto.ca>
2021-01-26 05:09:32 -0500

    plane function complete

--- ray-casting/src/Plane.cpp
+++ ray-casting/src/Plane.cpp
@@ -5,7 +5,26 @@ bool Plane::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
+  
+  // do projecton of the ray onto the plane
+  double projection;
+  double numerator = (point - ray.origin).dot(normal);
+  double demoinator = (ray.direction).dot(normal);
+
+  //check if value valid
+  if (demoinator == 0){
+    return false;
+  }
+
+  //calculate projection value
+  projection = numerator/demoinator;
+
+  if(projection >= min_t){
+    t = projection;
+    n = normal.normalized();
+    return true;
+  }
+
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }
