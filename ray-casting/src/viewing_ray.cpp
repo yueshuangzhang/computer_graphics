ad5ab5965613e72dc246c752cb1a378a4bb53096
yueshuangzhang <yueshuang.zhang@mail.utoronto.ca>
2021-01-27 02:22:39 -0500

    command update

--- ray-casting/src/viewing_ray.cpp
+++ ray-casting/src/viewing_ray.cpp
@@ -15,11 +15,12 @@ void viewing_ray(
   ray.origin = camera.e;
 
   // ray direction
-  //u = width/n_x * (j + 1/2) - width/2
-  //v = height/n_y * (i + 1/2) - width/2
+  //u = width/n_x * (j(col index) + 1/2) - width/2
+  //v = height/n_y * (i(row index) + 1/2) - width/2
 
   double u = camera.width / width * (j + 0.5) - (camera.width / 2);
-  double v = (-1) * camera.height / height * (i + 0.5) + (camera.height / 2);
+  // -1 since orgin top left
+  double v = (-1) * (camera.height / height * (i + 0.5) - (camera.height / 2));
 
   // ray = e + t(u(i)u + v(j)v + -dw)
   ray.direction = u * camera.u + v * camera.v - camera.d * camera.w;
