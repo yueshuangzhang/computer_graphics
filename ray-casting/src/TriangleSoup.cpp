3ab10269caf3336ab3d506e84fc8e6f975fd6300
yueshuangzhang <yueshuang.zhang@mail.utoronto.ca>
2021-01-27 01:01:29 -0500

    File upload

--- ray-casting/src/TriangleSoup.cpp
+++ ray-casting/src/TriangleSoup.cpp
@@ -7,7 +7,19 @@ bool TriangleSoup::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
+  // find the first hit object in the soup by calling the first hit helper function
+  int hit_id = -1;
+  double hit_t;
+  Eigen::Vector3d hit_n;
+  
+  bool hit = first_hit(ray, min_t, triangles, hit_id, hit_t, hit_n);
+
+  if(hit){
+    t = hit_t;
+    n = hit_n;
+    return true;
+  }
+
   return false;
   ////////////////////////////////////////////////////////////////////////////
 }
