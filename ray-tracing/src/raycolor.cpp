f6adb9812b52b3e9392c8389c9853c746980b2c2
yueshuangzhang <yueshuang.zhang@mail.utoronto.ca>
2021-01-31 18:09:18 -0500

    File upload

--- ray-tracing/src/raycolor.cpp
+++ ray-tracing/src/raycolor.cpp
@@ -20,11 +20,28 @@ bool raycolor(
   Eigen::Vector3d n;
   bool hit = first_hit(ray, min_t, objects, hit_id, t, n);
 
+  if (num_recursive_calls > 10){
+    return false;
+  }
+
   if (hit){
 
-    // get rgb
+    // get blinn_phong_shading rgb
     rgb = rgb + blinn_phong_shading(ray, hit_id, t, n, objects, lights);;
     
+    // get reflect ray
+    Ray reflect_ray;
+    reflect_ray.origin = ray.origin + t * ray.direction;
+    reflect_ray.direction = reflect(ray.direction, n);
+
+    // get reflect rgb
+    Eigen::Vector3d reflect_rgb;
+    bool recursive_raycolor;
+    recursive_raycolor = raycolor(reflect_ray, 1.0e-6, objects, lights, num_recursive_calls+1, reflect_rgb);
+    if (recursive_raycolor){
+      rgb += (objects[hit_id]->material->km.array()*reflect_rgb.array()).matrix();
+    }
+    return true;
   }
 
   return false;
