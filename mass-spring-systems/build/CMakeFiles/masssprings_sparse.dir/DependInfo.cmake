25aba81893cfcc8a95a555ce91e039ca9edf1ea5
yueshuangzhang <70617414+yueshuangzhang@users.noreply.github.com>
2021-03-24 00:35:41 -0400

    new assignment

---
+++ mass-spring-systems/build/CMakeFiles/masssprings_sparse.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Zoe/Desktop/Year_4_term_2/CSC317/Assignment/computer_graphics/mass-spring-systems/main.cpp" "/Users/Zoe/Desktop/Year_4_term_2/CSC317/Assignment/computer_graphics/mass-spring-systems/build/CMakeFiles/masssprings_sparse.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../json"
+  "../libigl/external/glad/include"
+  "../libigl/external/glfw/include"
+  "../libigl/external/stb"
+  "stb_image"
+  "../libigl/cmake/../include"
+  "../libigl/cmake/../external/eigen"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/Zoe/Desktop/Year_4_term_2/CSC317/Assignment/computer_graphics/mass-spring-systems/build/CMakeFiles/lib.dir/DependInfo.cmake"
+  "/Users/Zoe/Desktop/Year_4_term_2/CSC317/Assignment/computer_graphics/mass-spring-systems/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/Zoe/Desktop/Year_4_term_2/CSC317/Assignment/computer_graphics/mass-spring-systems/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/Users/Zoe/Desktop/Year_4_term_2/CSC317/Assignment/computer_graphics/mass-spring-systems/build/stb_image/CMakeFiles/igl_stb_image.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
